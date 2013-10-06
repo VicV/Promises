5cfe8a7dee046840fff7ee543d0452172bab165c
VicV <victorvuc@gmail.com>
2013-10-05 21:26:12 -0400

    Learned about scheduledFutures. Cool!

--- src/com/vicv/promises/util/PromiseUtils.java
+++ src/com/vicv/promises/util/PromiseUtils.java
@@ -2,6 +2,7 @@ package com.vicv.promises.util;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -9,24 +10,27 @@ import com.vicv.promises.Promise;
 
 public class PromiseUtils {
 
-	private static final ScheduledExecutorService worker = Executors.newSingleThreadScheduledExecutor();
+	private static final ScheduledExecutorService worker = Executors
+			.newSingleThreadScheduledExecutor();
+	
+	
+	private volatile static ScheduledFuture<?> futureTask;
 
-	// Fails a promise after a set time.
-	//http://stackoverflow.com/questions/3072173/how-to-call-a-method-after-a-delay
-	public static <T> Promise<T> timeoutPromise(final Promise<T> initialPromise, int millis) {
-		
-		Promise<T> failingPromise = initialPromise;
-		
-		Runnable futureTask = new Runnable() {
-			
+	// Fails a promise after a set time. (I miss android handlers!)
+	// http://stackoverflow.com/questions/3072173/how-to-call-a-method-after-a-delay
+	// http://stackoverflow.com/questions/7269294/how-to-stop-a-runnable-scheduled-for-repeated-execution-after-a-certain-number-o
+	public static <T> Promise<T> timeoutPromise(
+			final Promise<T> initialPromise, int millis) {
+
+
+		 futureTask = worker.schedule(new Runnable() {
+
 			@Override
 			public void run() {
 				initialPromise.fail(new TimeoutException());
 			}
-		};
-		
-		worker.schedule(futureTask, millis, TimeUnit.MILLISECONDS);
-		
-		return initialPromise; 
+		}, millis, TimeUnit.MILLISECONDS);
+
+		return initialPromise;
 	}
 }
