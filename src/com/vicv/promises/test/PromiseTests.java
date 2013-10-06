a11e47f50f4a4c243cc7e58f0baa03ef2c4dbce4
VicV <victorvuc@gmail.com>
2013-10-06 02:22:36 -0400

    Formatting

--- src/com/vicv/promises/test/PromiseTests.java
+++ src/com/vicv/promises/test/PromiseTests.java
@@ -1,6 +1,6 @@
 package com.vicv.promises.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.concurrent.TimeoutException;
